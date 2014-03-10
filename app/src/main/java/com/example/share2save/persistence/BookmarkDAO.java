cb3cfa6f791b6f24e3b4196f46558ad1472b16fb
joshuadickerson <joshuajdickerson@gmail.com>
2014-03-09 20:37:51 -0400

    working on sql lite

--- app/src/main/java/com/example/share2save/persistence/BookmarkDAO.java
+++ app/src/main/java/com/example/share2save/persistence/BookmarkDAO.java
@@ -11,21 +11,20 @@ import com.example.share2save.model.Bookmark;
  * Created by josh on 3/9/14.
  */
 public class BookmarkDAO extends SQLiteOpenHelper {
-
-    // All Static variables
-    // Database Version
+    // tutorial: http://www.androidhive.info/2011/11/android-sqlite-database-tutorial/
+    
     private static final int DATABASE_VERSION = 1;
-
-    // Database Name
     private static final String DATABASE_NAME = "sharetosave";
 
-    // Contacts table name
     private static final String TABLE_BOOKMARKS = "bookmarks";
+    private static final String BOOKMARKS_ID = "id";
+    private static final String BOOKMARKS_URL = "url";
+    private static final String BOOKMARKS_TITLE = "title";
 
-    // Contacts Table Columns names
-    private static final String KEY_ID = "id";
-    private static final String KEY_URL = "url";
-    private static final String KEY_TITLE = "title";
+    private static final String TABLE_TAGS = "tags";
+    private static final String TAGS_ID = "id";
+    private static final String TAGS_TAG = "tag";
+    private static final String TAGS_BOOKMARKS_ID = "fkBookmarkId";
 
     public BookmarkDAO(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -37,14 +36,24 @@ public class BookmarkDAO extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_BOOKMARKS + "(" +KEY_ID + " INTEGER PRIMARY KEY,"
-                + KEY_URL + " TEXT," + KEY_TITLE + " TEXT)";
+        String CREATE_BOOKMARK = "CREATE TABLE " + TABLE_BOOKMARKS + "(" + BOOKMARKS_ID + " INTEGER PRIMARY KEY,"
+                + BOOKMARKS_URL + " TEXT," + BOOKMARKS_TITLE + " TEXT)";
 
-        db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_BOOKMARK);
+
+        String CREATE_TAG = "CREATE TABLE "+TABLE_TAGS+" (" +
+                TAGS_ID + " INTEGER PRIMARY KEY, " +
+                TAGS_TAG + " TEXT, " +
+                TAGS_BOOKMARKS_ID + " INTEGER," +
+                "FOREIGN KEY("+TAGS_BOOKMARKS_ID+") REFERENCES " + TABLE_BOOKMARKS + "(" + BOOKMARKS_ID + ")" +
+                ");";
+
+        db.execSQL(CREATE_TAG);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOKMARKS);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOKMARKS);
         onCreate(db);
     }
